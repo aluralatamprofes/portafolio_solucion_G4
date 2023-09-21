65c07ec19fad21b78c9c43e2ffbe4663d46f1f94
Bruno Souza <brunoezully@gmail.com>
2023-09-21 12:30:30 -0300

    feat: animación de desplazamiento

---
+++ scripts/animation.js
@@ -0,0 +1,14 @@
+// Smooth scroll animation
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        const targetId = this.getAttribute('href').substring(1);
+        const targetElement = document.getElementById(targetId);
+
+        window.scrollTo({
+            top: targetElement.offsetTop,
+            behavior: 'smooth'
+        });
+    });
+});
